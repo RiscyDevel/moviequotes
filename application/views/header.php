4717f36372d1e3d065b5fc5beba12614d3a2fa9e
Risc <nipten@gmail.com>
2016-01-10 20:25:24 +0000

    Added some css styling to the site Added better error reporting.

--- application/views/header.php
+++ application/views/header.php
@@ -2,18 +2,21 @@
 <html lang="en-GB">
 	<head>
 		<meta charset="utf-8">
-		<title>MovieQuotes</title>
+		<title>MQ</title>
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<base href="/">
-		<link rel="stylesheet" href="/css/normal.css" type="text/css" media="all" />
+		<link href='https://fonts.googleapis.com/css?family=Lato:400,900' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="/css/normalize.css" type="text/css" media="all" />
 		<link rel="stylesheet" href="/css/moviequotes.css" type="text/css" media="all" />
-		<script src="/js/angular.min.js"></script>
-		<script src="/js/angular-animate.min.js"></script>
-		<script src="/js/moviequotes.js"></script>
 	</head>
 	<body>
 		<header>
-			<h1><a href="/">MQ</a></h1>
-			<nav><a href="/">List all quotes</a> <a href="/quotes/random">View random quote</a> <a href="/download/all">Download all quotes</a></nav>
+			<h1><a href="/"><span class="m">M</span>Q</a></h1>
+			<nav><a href="/">list all quotes</a> <a href="/quotes/random">random quote</a> <a href="/download/all">download all quotes</a></nav>
+			<?php
+if (isset($error)) {
+?><div class="error"><?php echo $error; ?></div><?php
+}
+?>
 		</header>
